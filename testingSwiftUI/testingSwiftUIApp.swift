bd6b2abc5fd0d96fcd57eb78ca33ed538c39ea02
chathurajanithya <it21263880@my.sliit.lk>
2025-05-15 00:42:46 +0530

    Initial Commit

---
+++ testingSwiftUI/testingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftUIApp.swift
+//  testingSwiftUI
+//
+//  Created by Chathura Janithya on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct testingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testingSwiftUITests/testingSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  testingSwiftUITests.swift
+//  testingSwiftUITests
+//
+//  Created by Chathura Janithya on 2025-05-15.
+//
+
+import Testing
+@testable import testingSwiftUI
+
+struct testingSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
